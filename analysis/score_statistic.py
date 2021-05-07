0a7faebc331048cc40c77888691ae45cefb50bac
zhouhy0903 <zhy990903@126.com>
2021-05-07 15:36:06 +0800

    plot_channel_psd

---
+++ analysis/score_statistic.py
@@ -0,0 +1,144 @@
+from score import get_score
+from state import get_state
+import matplotlib.pyplot as plt
+import traceback
+import mne
+import numpy as np
+from eegprocess import get_raw_eeg,get_epoch_eeg
+def draw_score_distribution():
+    score=[]
+    for i in range(1,60):
+        try:
+            score.extend(get_score(i)[0])
+        except Exception as e:
+            pass
+    print(score)
+    plt.hist(score,width=0.8,color="orange")
+    plt.title("shot score distribution")
+    plt.xlabel("shot score")
+    plt.ylabel("num")
+    plt.show()
+
+def discover_psd_difference():
+    highscore_epoch=None
+    lowscore_epoch=None
+    highscorenum=0
+    lowscorenum=0
+    for i in range(1,60):
+        try:
+            data_score=get_score(i)[0]
+            data_state=get_state(i,3)
+            data_eeg=get_epoch_eeg(i).drop(["condition"],axis=1)
+            data_raw_eeg=get_raw_eeg(i)
+            if not (len(data_score)==len(data_eeg["epoch"].value_counts()) and len(data_score)==len(data_state[data_state["markerText"]=="ShotOps"])): continue
+            print("yes")
+            
+            events,event_id=mne.events_from_annotations(data_raw_eeg)
+            #print(events)
+            epochs=mne.Epochs(data_raw_eeg,events,event_id,tmin=-3,tmax=0,event_repeated='drop',preload=True)
+            first,second=get_score(i+1)
+            rest_eeg=epochs["s1001"][0]
+            total_shot_num=len(first)+len(second)
+            first_shoot_eeg=epochs["s1002"][-total_shot_num:-len(second)]
+            for j in range(len(data_score)):
+                if data_score[j]>9:
+                    if highscore_epoch==None:
+                        highscore_epoch=first_shoot_eeg["s1002"][j]
+                    else:
+                        highscore_epoch=mne.concatenate_epochs([highscore_epoch,first_shoot_eeg["s1002"][j]])
+                    highscorenum+=1
+
+                if data_score[j]<8:
+                    if lowscore_epoch==None:
+                        lowscore_epoch=first_shoot_eeg["s1002"][j]
+                    else:
+                        lowscore_epoch=mne.concatenate_epochs([lowscore_epoch,first_shoot_eeg["s1002"][j]])
+                    lowscorenum+=1
+            print(highscorenum)
+            print(lowscorenum)
+            #fig1=highscore_epoch.plot_psd_topomap(show=False,normalize=True)
+            #fig2=lowscore_epoch.plot_psd_topomap(show=False,normalize=True)
+            #fig1.savefig(r"C:\Users\zhou\Desktop\毕业设计\mechanical\最终论文\pic\high_score_3s_test\high_score{}.jpg".format(i))
+            #fig2.savefig(r"C:\Users\zhou\Desktop\毕业设计\mechanical\最终论文\pic\low_score_3s_test\low_score{}.jpg".format(i))
+        except Exception as e:
+            traceback.print_exc()
+    fig1=highscore_epoch.plot_psd_topomap(show=False,cmap="RdBu_r",normalize=True)
+    fig2=lowscore_epoch.plot_psd_topomap(show=False,cmap="RdBu_r",normalize=True)
+    fig1.savefig(r"C:\Users\zhou\Desktop\毕业设计\mechanical\最终论文\pic\high_score.jpg")
+    fig2.savefig(r"C:\Users\zhou\Desktop\毕业设计\mechanical\最终论文\pic\low_score.jpg")
+    print(highscorenum)
+    print(lowscorenum)
+
+
+def discover_event_difference():
+    highscore_epoch=None
+    lowscore_epoch=None
+    highscorenum=0
+    lowscorenum=0
+    for i in range(1,60):
+        try:
+            data_score=get_score(i)[0]
+            data_state=get_state(i,3)
+            data_eeg=get_epoch_eeg(i).drop(["condition"],axis=1)
+            data_raw_eeg=get_raw_eeg(i)
+            if not (len(data_score)==len(data_eeg["epoch"].value_counts()) and len(data_score)==len(data_state[data_state["markerText"]=="ShotOps"])): continue
+            print("yes")
+            
+            events,event_id=mne.events_from_annotations(data_raw_eeg)
+            #print(events)
+            epochs=mne.Epochs(data_raw_eeg,events,event_id,tmin=-1,tmax=0,event_repeated='drop',preload=True)
+            first,second=get_score(i+1)
+            rest_eeg=epochs["s1001"][0]
+            total_shot_num=len(first)+len(second)
+            first_shoot_eeg=epochs["s1002"][-total_shot_num:-len(second)]
+            for j in range(len(data_score)):
+                if data_score[j]>9:
+                    if highscore_epoch==None:
+                        highscore_epoch=first_shoot_eeg["s1002"][j]
+                    else:
+                        highscore_epoch=mne.concatenate_epochs([highscore_epoch,first_shoot_eeg["s1002"][j]])
+                    highscorenum+=1
+
+                if data_score[j]<8:
+                    if lowscore_epoch==None:
+                        lowscore_epoch=first_shoot_eeg["s1002"][j]
+                    else:
+                        lowscore_epoch=mne.concatenate_epochs([lowscore_epoch,first_shoot_eeg["s1002"][j]])
+                    lowscorenum+=1
+            print(highscorenum)
+            print(lowscorenum)
+            #fig1=highscore_epoch.plot_psd_topomap(show=False,normalize=True)
+            #fig2=lowscore_epoch.plot_psd_topomap(show=False,normalize=True)
+            #fig1.savefig(r"C:\Users\zhou\Desktop\毕业设计\mechanical\最终论文\pic\high_score_3s_test\high_score{}.jpg".format(i))
+            #fig2.savefig(r"C:\Users\zhou\Desktop\毕业设计\mechanical\最终论文\pic\low_score_3s_test\low_score{}.jpg".format(i))
+        except Exception as e:
+            traceback.print_exc()
+
+    freq_low=[0,4,8,12,30]
+    freq_high=[4,8,12,30,45]
+    for j in range(len(freq_low)):
+        highscore_epoch.load_data().filter(l_freq=freq_low[j],h_freq=freq_high[j])
+        highscoredata=highscore_epoch.get_data()
+        corr_matrix=mne.connectivity.envelope_correlation(highscoredata,combine="mean")
+        plt.imshow(corr_matrix)
+        plt.colorbar()
+        plt.title("highscore_({}Hz-{}Hz)".format(freq_low[j],freq_high[j]))
+        plt.savefig(r"C:\Users\zhou\Desktop\毕业设计\mechanical\最终论文\pic\diff\corr\high_score\high_score({}Hz-{}Hz).jpg".format(freq_low[j],freq_high[j]))
+        plt.close()
+
+        lowscore_epoch.load_data().filter(l_freq=freq_low[j],h_freq=freq_high[j])
+        lowscoredata=lowscore_epoch.get_data()
+        corr_matrix=mne.connectivity.envelope_correlation(lowscoredata,combine="mean")
+        plt.imshow(corr_matrix)
+        plt.colorbar()
+        plt.title("lowscore_({}Hz-{}Hz)".format(freq_low[j],freq_high[j]))
+        plt.savefig(r"C:\Users\zhou\Desktop\毕业设计\mechanical\最终论文\pic\diff\corr\low_score\low_score({}Hz-{}Hz).jpg".format(freq_low[j],freq_high[j]))
+        plt.close()
+    #fig1.savefig(r"C:\Users\zhou\Desktop\毕业设计\mechanical\最终论文\pic\diff\event\high_score.jpg")
+    #fig2.savefig(r"C:\Users\zhou\Desktop\毕业设计\mechanical\最终论文\pic\diff\event\low_score.jpg")
+    print(highscorenum)
+    print(lowscorenum)
+
+
+#discover_psd_difference()
+discover_event_difference()
--- eeg_psd_map/plot_eeg.py
+++ eeg_psd_map/plot_eeg.py
@@ -6,14 +6,16 @@ import numpy as np
 import pandas as pd
 import re
 from mne.time_frequency import tfr_morlet, psd_multitaper, psd_welch
+from spectrum import Periodogram, TimeSeries
 
 #修改路径为去除眼电后.fif目录文件
-path_config=r""
+path_config=r"D:\data\jxy\data"
 path=os.path.join(path_config,"*.fif")
 save_path=os.path.join(path_config,"outcome")
 
 #需要提取的标签名称
 markernames=["s9003","s9004","s9005","s9006","s9007","s9008","s9009"]
+channelnames=["Fz","F3","F4","P3"]
 #功率谱脑地形图的上下幅值
 limit=[(0,0.9),(0,0.2),(0,0.9),(0,0.6),(-0.2,0.2)]
 
@@ -22,7 +24,7 @@ def init():
         os.makedirs(save_path)
 
 
-def read_fif():
+def save_psd():
     global path,save_path,markernames,limit
     files=glob(path)
     for f in files:
@@ -46,7 +48,45 @@ def read_fif():
                 cur_path=os.path.join(save_path_person,markername+"_"+band[0][2]+".jpg")
                 fig.savefig(cur_path)
 
+
+def save_channel_psd():
+    global path,save_path,markernames,channelnames
+    files=glob(path)
+    for f in files:
+        
+        data=mne.io.read_raw(f)
+        print("Reading from {}".format(f))
+        events,event_id=mne.events_from_annotations(data)
+        epochs=mne.Epochs(data,events,event_id,tmin=-3,tmax=3,event_repeated='drop',preload=True)[markernames]
+
+
+        for channelname in channelnames:
+            plot_channel_psd(epochs["s9003"],channelname)
+        break
+
+
+def plot_channel_psd(data_eeg,channel):
+    def get_band(data,chname):
+        p=Periodogram(data,sampling=500)
+        p.run()
+        frepsd=pd.DataFrame()
+        frepsd["freq"]=p.frequencies()
+        frepsd["psd"]=10*np.log10(p.psd)
+        frepsd.drop(frepsd[frepsd["freq"]>=45].index,inplace=True)
+
+        fig = plt.figure(1)
+        ax = plt.axes()
+        ax.spines['top'].set_visible(False)
+        ax.spines['right'].set_visible(False)
+        plt.plot(frepsd["freq"].tolist(),frepsd["psd"].tolist(),color="blue")
+        plt.xlabel("Frequency/Hz")
+        plt.ylabel("PSD $\mu V^2/Hz (dB)$")
+        plt.title(chname)
+        plt.show()
+    get_band(data_eeg.to_data_frame()[channel].tolist(),channel)
+
 init()
 
 if __name__=="__main__":
-    read_fif()
+    #save_psd()
+    save_channel_psd()
